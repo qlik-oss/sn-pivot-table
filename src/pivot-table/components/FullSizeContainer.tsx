4192ea4ab068da0c619060c1763247d8b3d04d2f
Christofer Bengtsson <cbt@qlik.com>
2022-02-08 14:40:17 +0100

    chore: update lint rule

--- src/pivot-table/components/FullSizeContainer.tsx
+++ src/pivot-table/components/FullSizeContainer.tsx
@@ -8,11 +8,11 @@ interface FullSizeContainerProps {
 
 const FullSizeContainer = ({ width, height, children }: FullSizeContainerProps): JSX.Element => (
   <div
-  style={{
-    display: 'block',
-    width,
-    height,
-  }}
+    style={{
+      display: 'block',
+      width,
+      height,
+    }}
   >
     {children}
   </div>
