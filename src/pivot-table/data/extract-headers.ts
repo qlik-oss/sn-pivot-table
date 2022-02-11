543f60969846719f4887b19ffe57c3f945f64081
Christofer Bengtsson <cbt@qlik.com>
2022-02-11 09:34:51 +0100

    refactor: simplify data extraction

--- src/pivot-table/data/extract-headers.ts
+++ src/pivot-table/data/extract-headers.ts
@@ -2,23 +2,15 @@ import { NxDimensionInfo } from '../../types/QIX';
 import { CellValue } from '../../types/types';
 
 const extractHeaders = (qDim: NxDimensionInfo[], rowCount: number, colCount: number): CellValue[][] => {
-  const ary: CellValue[][] = [];
-
-  for (let colIdx = 0; colIdx < colCount; colIdx += 1) {
-    for (let rowIdx = 0; rowIdx < rowCount; rowIdx += 1) {
-      if (!Array.isArray(ary[colIdx])) {
-        ary[colIdx] = [];
-      }
-
-      ary[colIdx][rowIdx] = null;
-    }
-  }
+  const matrix = Array(colCount)
+    .fill(null)
+    .map(() => Array(rowCount).fill(null));
 
   qDim.slice(0, colCount).forEach((info, colIdx) => {
-    ary[colIdx][rowCount - 1] = info.qFallbackTitle;
+    matrix[colIdx][rowCount - 1] = info.qFallbackTitle;
   });
 
-  return ary;
+  return matrix;
 };
 
 export default extractHeaders;
