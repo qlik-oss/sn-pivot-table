3cb61ed3573897ec0ae6ae9ee8688a3790ba8d47
Christofer Bengtsson <cbt@qlik.com>
2022-03-15 08:32:10 +0100

    perf: memoize measure text

--- src/hooks/__tests__/use-measure-text.test.ts
+++ src/hooks/__tests__/use-measure-text.test.ts
@@ -1,7 +1,7 @@
 import { renderHook } from '@testing-library/react-hooks';
 import useMeasureText from '../use-measure-text';
 
-jest.mock('../use-data-model');
+// jest.mock('../use-data-model');
 
 describe('useMeasureText', () => {
   let measureTextMock: jest.Mock<{ width: number }>;
@@ -24,13 +24,6 @@ describe('useMeasureText', () => {
       const { result } = renderHook(() => useMeasureText('13px', 'font'));
 
       expect(result.current.estimateWidth(2)).toBe(300);
-    });
-
-    test('should have a minimum width limit', () => {
-      measureTextMock.mockReturnValue({ width: 1 });
-      const { result } = renderHook(() => useMeasureText('13px', 'font'));
-
-      expect(result.current.estimateWidth(2)).toBe(100);
     });
   });
 
