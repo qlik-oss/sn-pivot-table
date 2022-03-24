749265ec103be7da4931df8c369d964a7524f44f
Christofer Bengtsson <cbt@qlik.com>
2022-03-24 13:18:03 +0100

    fix: list item key (#55)

--- src/pivot-table/components/helpers/get-item-key.ts
+++ src/pivot-table/components/helpers/get-item-key.ts
@@ -1,18 +1,5 @@
-import NxDimCellType from '../../../types/QIX';
 import { ListItemData } from '../../../types/types';
 
-const getItemKey = (index: number, data: ListItemData): string => {
-  const { qElemNo, qType } = data.list[index];
-
-  if (qElemNo < 0) {
-    return `${qElemNo}-${index}`;
-  }
-
-  if (qType === NxDimCellType.NX_DIM_CELL_PSEUDO) {
-    return `${NxDimCellType.NX_DIM_CELL_PSEUDO}-${index}`;
-  };
-
-  return qElemNo.toString();
-};
+const getItemKey = (index: number, data: ListItemData): string => `${data.list[index].qElemNo}-${index}`;
 
 export default getItemKey;
