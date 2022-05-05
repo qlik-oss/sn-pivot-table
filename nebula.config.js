554c529af7ea612ace5818133623502d1700c55e
Christofer Bengtsson <cbt@qlik.com>
2022-05-05 14:29:44 +0200

    docs: api spec (#90)

--- nebula.config.js
+++ nebula.config.js
@@ -1,5 +1,16 @@
+const path = require('path');
+
+const { version } = require(path.resolve(__dirname, './package.json')); // eslint-disable-line
+const mode = process.env.NODE_ENV === 'production' ? 'production' : 'development';
+const sourcemap = mode !== 'production';
+
 module.exports = {
   build: {
+    sourcemap,
+    mode,
     typescript: true,
+    replacementStrings: {
+      'process.env.PACKAGE_VERSION': JSON.stringify(version),
+    },
   },
 };
