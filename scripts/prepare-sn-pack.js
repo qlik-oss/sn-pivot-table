a82e85e33e3b163d68705ad36f99c22016458899
Christofer Bengtsson <cbt@qlik.com>
2022-05-09 11:10:44 +0200

    chore: update package.json

--- scripts/prepare-sn-pack.js
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env node
-
-const path = require('path');
-const fs = require('fs');
-const validate = require('./validate-nebula-package');
-
-const p = path.resolve(process.cwd(), 'package.json');
-const cleanedPkg = validate(require(p), process.cwd()); // eslint-disable-line
-fs.writeFileSync(p, cleanedPkg);
