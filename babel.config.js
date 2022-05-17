4b352a0a4eb8e630009c590c0d86d2d01d8fa54d
Christofer Bengtsson <cbt@qlik.com>
2022-05-17 07:24:49 +0200

    chore(deps): bump user-event to v14 (#108)

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,13 @@
-module.exports = {
-  presets: ['@babel/preset-env', '@babel/preset-react', '@babel/typescript'],
+module.exports = (api) => {
+  const isTest = api.env('test');
+  if (isTest) {
+    return {
+      presets: ['@babel/preset-env', '@babel/preset-react', '@babel/typescript'],
+      targets: { node: 'current' },
+    };
+  }
+
+  return {
+    presets: ['@babel/preset-env', '@babel/preset-react', '@babel/typescript'],
+  };
 };
