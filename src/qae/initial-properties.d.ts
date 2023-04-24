d34f86694974ff962a8c78eb6b59a2fb7c868c20
a-m-dev <a.mirzaei.dev@gmail.com>
2023-04-24 15:17:40 +0200

    fix: lint issue

--- src/qae/initial-properties.d.ts
+++ src/qae/initial-properties.d.ts
@@ -1,35 +1,35 @@
 import { Component } from "../types/QIX";
 
-declare module "initial-properties" {
-  declare const properties: {
-    version: number;
-    qHyperCubeDef: {
-      qAlwaysFullyExpanded: boolean;
-      qDimensions: EngineAPI.INxDimension[];
-      qMeasures: EngineAPI.INxMeasure[];
-      qMode: "P";
-      qSuppressMissing: boolean;
-      qSuppressZero: boolean;
-      qShowTotalsAbove: boolean;
-      qInitialDataFetch: [
-        {
-          qTop: 0;
-          qLeft: 0;
-          qWidth: 50;
-          qHeight: 50;
-        }
-      ];
-      qIndentMode: boolean;
-    };
-    showTitles: boolean;
-    title: "";
-    subtitle: "";
-    footnote: "";
-    nullValueRepresentation: {
-      text: "-";
-    };
-    components?: Component[];
+declare module "initial-properties";
+
+declare const properties: {
+  version: number;
+  qHyperCubeDef: {
+    qAlwaysFullyExpanded: boolean;
+    qDimensions: EngineAPI.INxDimension[];
+    qMeasures: EngineAPI.INxMeasure[];
+    qMode: "P";
+    qSuppressMissing: boolean;
+    qSuppressZero: boolean;
+    qShowTotalsAbove: boolean;
+    qInitialDataFetch: [
+      {
+        qTop: 0;
+        qLeft: 0;
+        qWidth: 50;
+        qHeight: 50;
+      }
+    ];
+    qIndentMode: boolean;
   };
-}
+  showTitles: boolean;
+  title: "";
+  subtitle: "";
+  footnote: "";
+  nullValueRepresentation: {
+    text: "-";
+  };
+  components?: Component[];
+};
 
 export default properties;
