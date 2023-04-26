f36a35ad49a923aca05403534e747cfea04904c4
Christofer Bengtsson <cbt@qlik.com>
2023-04-26 14:37:47 +0200

    fix: expand or collapse data fetch (#245)

--- src/hooks/use-load-data-pages.ts
+++ src/hooks/use-load-data-pages.ts
@@ -9,75 +9,6 @@ interface UseLoadDataPages {
   qPivotDataPages: EngineAPI.INxPivotPage[];
 }
 
-const MAX_GRID_SIZE = 10000;
-
-const safeGuardGridSize = (viewService: ViewService): boolean => {
-  const { gridWidth, gridHeight } = viewService;
-  if (!gridWidth) return true;
-  if (!gridHeight) return true;
-  if (gridWidth < 0) return true;
-  if (gridHeight < 0) return true;
-  return false;
-};
-
-const getPagesToTheTop = (viewService: ViewService, maxHeight: number): EngineAPI.IRect[] => {
-  const { gridColumnStartIndex, gridRowStartIndex, gridWidth, gridHeight } = viewService;
-  const pages = [] as EngineAPI.IRect[];
-
-  if (safeGuardGridSize(viewService)) {
-    // handle zero size and when viewService has not yet been initialized
-    return pages;
-  }
-
-  const totalHeight = Math.min(gridRowStartIndex + gridHeight * 2, maxHeight);
-  const batchSize = Math.floor(MAX_GRID_SIZE / gridWidth);
-  let batchTop = Math.max(0, totalHeight - batchSize);
-  let batchHeight = totalHeight - batchTop;
-
-  do {
-    pages.unshift({
-      qLeft: gridColumnStartIndex,
-      qTop: Math.max(0, batchTop),
-      qWidth: gridWidth,
-      qHeight: batchHeight,
-    });
-
-    batchHeight = Math.min(batchHeight, batchTop);
-    batchTop -= batchSize;
-  } while (pages[0]?.qTop > 0);
-
-  return pages;
-};
-
-const getPagesToTheLeft = (viewService: ViewService, maxWidth: number): EngineAPI.IRect[] => {
-  const { gridColumnStartIndex, gridRowStartIndex, gridWidth, gridHeight } = viewService;
-  const pages = [] as EngineAPI.IRect[];
-
-  if (safeGuardGridSize(viewService)) {
-    // handle zero size and when viewService has not yet been initialized
-    return pages;
-  }
-
-  const totalWidth = Math.min(gridColumnStartIndex + gridWidth * 2, maxWidth);
-  const batchSize = Math.floor(MAX_GRID_SIZE / gridHeight);
-  let batchLeft = Math.max(0, totalWidth - batchSize);
-  let batchWidth = totalWidth - batchLeft;
-
-  do {
-    pages.unshift({
-      qLeft: Math.max(0, batchLeft),
-      qTop: gridRowStartIndex,
-      qWidth: batchWidth,
-      qHeight: gridHeight,
-    });
-
-    batchWidth = Math.min(batchWidth, batchLeft);
-    batchLeft -= batchSize;
-  } while (pages[0]?.qLeft > 0);
-
-  return pages;
-};
-
 const shouldFetchAdditionalData = (
   qLastExpandedPos: EngineAPI.INxCellPosition | undefined,
   viewService: ViewService
@@ -105,10 +36,16 @@ const useLoadDataPages = (model: Model, layoutService: LayoutService, viewServic
     if ((model as EngineAPI.IGenericObject)?.getHyperCubePivotData && !snapshotData) {
       // If a cell have been expanded. Fetch data to the last scrolled position.
       if (shouldFetchAdditionalData(qLastExpandedPos, viewService)) {
-        const pages = [...getPagesToTheLeft(viewService, qSize.qcx), ...getPagesToTheTop(viewService, qSize.qcy)];
-
         try {
-          const pivotPages = await (model as EngineAPI.IGenericObject).getHyperCubePivotData(Q_PATH, pages);
+          const pivotPages = await (model as EngineAPI.IGenericObject).getHyperCubePivotData(Q_PATH, [
+            {
+              qLeft: viewService.gridColumnStartIndex,
+              qTop: viewService.gridRowStartIndex,
+              qWidth: viewService.gridWidth,
+              qHeight: viewService.gridHeight,
+            },
+          ]);
+
           setDataPages(pivotPages);
         } catch (error) {
           // TODO handle error
