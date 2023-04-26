44353796e73347e16f3f8fedf2cfb05936c57fb8
Max Gefvert <max.gefvert@qlik.com>
2023-04-26 11:00:07 +0200

    fix: less generalization

--- src/pivot-table/contexts/create-styling.ts
+++ src/pivot-table/contexts/create-styling.ts
@@ -11,11 +11,7 @@ export const DEFAULT_BACKGROUND_COLOR = "transparent";
 export const DEFAULT_ROW_HEIGHT = 28;
 export const DEFAULT_LINE_CLAMP = 1;
 
-interface StyleProperties {
-  color?: string;
-  fontSize?: string;
-  fontFamily?: string;
-}
+type StyleProperties = Record<string, string>;
 
 export interface Styling {
   header: StyleProperties;
@@ -28,69 +24,58 @@ export interface Styling {
 enum Path {
   HEADER = "header",
   CONTENT = "content",
-  BASE = "",
+  ROOT = "",
 }
-enum ComponentAttribute {
+enum Attribute {
   FONT_SIZE = "fontSize",
   FONT_FAMILY = "fontFamily",
   COLOR = "color",
-}
-
-enum BaseAttribute {
   CELL_HEIGHT = "cellHeight",
   LINE_CLAMP = "lineClamp",
 }
 
-type ComponentPath = Path.HEADER | Path.CONTENT;
+const resolveFontSize = (fontSize: number | undefined) => (fontSize ? `${fontSize}px` : undefined);
 
-const THEME_ATTRIBUTES = [
-  { path: Path.HEADER, attribute: ComponentAttribute.FONT_SIZE, defaultValue: DEFAULT_FONT_SIZE },
-  { path: Path.HEADER, attribute: ComponentAttribute.FONT_FAMILY, defaultValue: DEFAULT_FONT_FAMILY },
-  { path: Path.HEADER, attribute: ComponentAttribute.COLOR, defaultValue: DEFAULT_FONT_COLOR },
-  { path: Path.CONTENT, attribute: ComponentAttribute.FONT_SIZE, defaultValue: DEFAULT_FONT_SIZE },
-  { path: Path.CONTENT, attribute: ComponentAttribute.FONT_FAMILY, defaultValue: DEFAULT_FONT_FAMILY },
-  { path: Path.CONTENT, attribute: ComponentAttribute.COLOR, defaultValue: DEFAULT_FONT_COLOR },
-  // { path: Path.BASE, attribute: Attribute.BG_COLOR, defaultValue: DEFAULT_BACKGROUND_COLOR },
-  { path: Path.BASE, attribute: BaseAttribute.CELL_HEIGHT, defaultValue: DEFAULT_LINE_CLAMP },
-];
+/**
+ * get the styling for header and content
+ */
+const createComponentStyling = (
+  chartStyling: Component | undefined,
+  theme: stardust.Theme,
+  path: Path.HEADER | Path.CONTENT
+) => {
+  const fontSize =
+    resolveFontSize(chartStyling?.[path]?.fontSize) ??
+    theme.getStyle(BASE_PATH, path, Attribute.FONT_SIZE) ??
+    DEFAULT_FONT_SIZE;
 
-const getLayoutValue = (chartStyling: Component | undefined, path: ComponentPath, attribute: ComponentAttribute) => {
-  switch (attribute) {
-    case ComponentAttribute.COLOR:
-      return chartStyling?.[path]?.fontColor?.color ?? undefined; // TODO: resolve color
-    case ComponentAttribute.FONT_SIZE:
-      const fontSize = chartStyling?.[path]?.fontSize;
-      return fontSize ? `${fontSize}px` : undefined;
-    case ComponentAttribute.FONT_FAMILY:
-      return chartStyling?.[path]?.fontFamily;
-    default:
-      break;
-  }
+  const fontFamily =
+    chartStyling?.[path]?.fontFamily ?? theme.getStyle(BASE_PATH, path, Attribute.FONT_FAMILY) ?? DEFAULT_FONT_SIZE;
+
+  const color =
+    chartStyling?.[path]?.fontColor?.color ?? // TODO: resolve color
+    theme.getStyle(BASE_PATH, path, Attribute.COLOR) ??
+    DEFAULT_FONT_COLOR;
+
+  return { fontSize, fontFamily, color };
 };
 
+/**
+ * creates the styling based on layout, theme and default values - in that order
+ */
 export const createStyling = (layoutService: LayoutService, theme: stardust.Theme): Styling => {
+  1;
   const chartStyling = layoutService.layout.components?.find((n) => n.key === "theme");
-  const styling: Styling = {
-    header: {},
-    content: {},
-    backgroundColor: DEFAULT_BACKGROUND_COLOR,
-    cellHeight: DEFAULT_ROW_HEIGHT,
-    lineClamp: DEFAULT_LINE_CLAMP,
-  };
 
-  THEME_ATTRIBUTES.forEach((themeAttribute) => {
-    const { path, attribute, defaultValue } = themeAttribute;
-    const fallbackValue = theme.getStyle(BASE_PATH, path, attribute) ?? defaultValue;
+  const lineClamp =
+    chartStyling?.rowHeight?.linesCount ??
+    // theme.getStyle(BASE_PATH, Path.ROOT, Attribute.LINE_CLAMP) ?? // TODO: add line clamp to theme
+    DEFAULT_LINE_CLAMP;
+  const cellHeight = lineClamp * DEFAULT_ROW_HEIGHT;
+  const backgroundColor = DEFAULT_BACKGROUND_COLOR; // TODO: Add  background to styling panel?
 
-    if (path === Path.BASE) {
-      const lineClamp = chartStyling?.rowHeight?.linesCount ?? (fallbackValue as number);
-      styling[BaseAttribute.LINE_CLAMP] = lineClamp;
-      styling[BaseAttribute.CELL_HEIGHT] = lineClamp * DEFAULT_ROW_HEIGHT;
-    } else {
-      const attr = attribute as ComponentAttribute;
-      styling[path][attr] = getLayoutValue(chartStyling, path, attr) ?? (fallbackValue as string);
-    }
-  });
+  const header = createComponentStyling(chartStyling, theme, Path.HEADER);
+  const content = createComponentStyling(chartStyling, theme, Path.CONTENT);
 
-  return styling;
+  return { header, content, lineClamp, cellHeight, backgroundColor };
 };
