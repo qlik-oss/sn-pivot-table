7b024561d4b4515d90707d60511bcbd7cb2e2f65
Christofer Bengtsson <cbt@qlik.com>
2023-05-03 10:44:44 +0200

    ci: locale verify job

--- src/hooks/use-translations.ts
+++ src/hooks/use-translations.ts
@@ -1,12 +1,12 @@
-import { useMemo, useTranslator } from "@nebula.js/stardust";
+import { useEffect, useTranslator } from "@nebula.js/stardust";
 import registerLocale from "../locale/src";
 import { ExtendedTranslator } from "../types/types";
 
 export const useTranslations = () => {
   const translator = useTranslator() as ExtendedTranslator;
   const language = translator.language();
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  useMemo(() => registerLocale(translator), [translator, language]);
+
+  useEffect(() => registerLocale(translator), [translator, language]);
 
   return { translator, language };
 };
