c7060d7ce3a30b5603346a86e57251cc98574ad4
a-m-dev <a.mirzaei.dev@gmail.com>
2023-09-01 14:50:35 +0200

    test: add unit test

---
+++ src/pivot-table/hooks/__tests__/use-sorting.test.ts
@@ -0,0 +1,217 @@
+import { renderHook } from "@testing-library/react";
+import type { HyperCube, Model } from "../../../types/QIX";
+import type { Header, SortDirection } from "../../../types/types";
+import useSorting from "../use-sorting";
+
+describe("use-sorting", () => {
+  let model: Model;
+  let applyPatches: jest.MockedFunction<() => Promise<void>>;
+  let hyperCube: HyperCube;
+
+  let header: Header;
+  let newSortDirection: SortDirection;
+
+  const renderer = () => renderHook(() => useSorting(model, hyperCube)).result.current;
+
+  beforeEach(() => {
+    applyPatches = jest.fn();
+    model = { applyPatches } as unknown as Model;
+    hyperCube = { qDimensionInfo: [{}, {}] } as unknown as HyperCube;
+
+    header = { isDim: true, colIdx: 0, qReverseSort: false } as Header;
+    newSortDirection = "A";
+  });
+
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
+
+  describe("changeSortOrder", () => {
+    test("should throw error if there was no model", async () => {
+      model = undefined as unknown as Model;
+
+      const { changeSortOrder } = renderer();
+      try {
+        await changeSortOrder(header, newSortDirection);
+      } catch (err: any) {
+        expect(err.message).toBe("No Model provided!");
+      }
+    });
+
+    test("should call applyPatches with correct column index for measures", async () => {
+      const { changeSortOrder } = renderer();
+      newSortDirection = "D";
+      header = { isDim: false, colIdx: 5 } as Header;
+      await changeSortOrder(header, newSortDirection);
+      expect(applyPatches).toHaveBeenCalledTimes(1);
+      expect(applyPatches).toHaveBeenCalledWith(
+        [
+          {
+            qOp: "Replace",
+            qPath: `/qHyperCubeDef/qMeasures/${header.colIdx - hyperCube.qDimensionInfo.length}/qDef/qReverseSort`,
+            qValue: String(!header.qReverseSort),
+          },
+        ],
+        true,
+      );
+    });
+
+    test("should call applyPatches with correct patch object for Ascensing sort", async () => {
+      const { changeSortOrder } = renderer();
+      newSortDirection = "A";
+      header.qReverseSort = true;
+      await changeSortOrder(header, newSortDirection);
+      expect(applyPatches).toHaveBeenCalledTimes(1);
+      expect(applyPatches).toHaveBeenCalledWith(
+        [
+          {
+            qOp: "Replace",
+            qPath: `/qHyperCubeDef/qDimensions/${header.colIdx}/qDef/qReverseSort`,
+            qValue: String(!header.qReverseSort),
+          },
+        ],
+        true,
+      );
+    });
+
+    test("should call applyPatches with correct patch object for Descending sort", async () => {
+      const { changeSortOrder } = renderer();
+      newSortDirection = "D";
+      await changeSortOrder(header, newSortDirection);
+      expect(applyPatches).toHaveBeenCalledTimes(1);
+      expect(applyPatches).toHaveBeenCalledWith(
+        [
+          {
+            qOp: "Replace",
+            qPath: `/qHyperCubeDef/qDimensions/${header.colIdx}/qDef/qReverseSort`,
+            qValue: String(!header.qReverseSort),
+          },
+        ],
+        true,
+      );
+    });
+
+    test("should return false and log error if applyPatches fails", async () => {
+      jest.spyOn(console, "error");
+      applyPatches.mockRejectedValue("failure");
+      const { changeSortOrder } = renderer();
+      newSortDirection = "A";
+      const result = await changeSortOrder(header, newSortDirection);
+      expect(applyPatches).toHaveBeenCalledTimes(1);
+      expect(result).toBeFalsy;
+      expect(global.console.error).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe("changeActivelySortedHeader", () => {
+    let expectedHeaderObject: Header;
+    beforeEach(() => {
+      header = {
+        ...header,
+        qLibraryId: "#someLibraryId",
+        fieldId: "#someFieldId",
+      };
+      expectedHeaderObject = {
+        colIdx: header.colIdx,
+        qLibraryId: header.qLibraryId,
+        fieldId: header.fieldId,
+        sortDirection: header.sortDirection,
+      } as Header;
+    });
+
+    test("should throw error if there was no model", async () => {
+      model = undefined as unknown as Model;
+
+      const { changeActivelySortedHeader } = renderer();
+      try {
+        await changeActivelySortedHeader(header);
+      } catch (err: any) {
+        expect(err.message).toBe("No Model provided!");
+      }
+    });
+
+    test("should call applyPatches using correct operation if `activelySortedColumn` is already exists on hypercube", async () => {
+      hyperCube = { ...hyperCube, activelySortedColumn: {} } as HyperCube;
+      const { changeActivelySortedHeader } = renderer();
+      newSortDirection = "D";
+      header.isDim = false;
+      await changeActivelySortedHeader(header);
+      expect(applyPatches).toHaveBeenCalledTimes(1);
+      expect(applyPatches).toHaveBeenCalledWith(
+        [
+          {
+            qOp: "Replace",
+            qPath: "/qHyperCubeDef/activelySortedColumn",
+            qValue: JSON.stringify({ ...expectedHeaderObject, colIdx: header.colIdx }),
+          },
+        ],
+        true,
+      );
+    });
+
+    test("should call applyPatches with correct column index for measures", async () => {
+      const { changeActivelySortedHeader } = renderer();
+      newSortDirection = "D";
+      header.isDim = false;
+      header.colIdx = 5;
+      await changeActivelySortedHeader(header);
+      expect(applyPatches).toHaveBeenCalledTimes(1);
+      expect(applyPatches).toHaveBeenCalledWith(
+        [
+          {
+            qOp: "Add",
+            qPath: "/qHyperCubeDef/activelySortedColumn",
+            qValue: JSON.stringify({ ...expectedHeaderObject, colIdx: header.colIdx }),
+          },
+        ],
+        true,
+      );
+    });
+
+    test("should call applyPatches with correct patch object for Ascensing sort", async () => {
+      const { changeActivelySortedHeader } = renderer();
+      newSortDirection = "A";
+      header.qReverseSort = true;
+      await changeActivelySortedHeader(header);
+      expect(applyPatches).toHaveBeenCalledTimes(1);
+      expect(applyPatches).toHaveBeenCalledWith(
+        [
+          {
+            qOp: "Add",
+            qPath: "/qHyperCubeDef/activelySortedColumn",
+            qValue: JSON.stringify(expectedHeaderObject),
+          },
+        ],
+        true,
+      );
+    });
+
+    test("should call applyPatches with correct patch object for Descending sort", async () => {
+      const { changeActivelySortedHeader } = renderer();
+      newSortDirection = "D";
+      await changeActivelySortedHeader(header);
+      expect(applyPatches).toHaveBeenCalledTimes(1);
+      expect(applyPatches).toHaveBeenCalledWith(
+        [
+          {
+            qOp: "Add",
+            qPath: "/qHyperCubeDef/activelySortedColumn",
+            qValue: JSON.stringify(expectedHeaderObject),
+          },
+        ],
+        true,
+      );
+    });
+
+    test("should return false and log error if applyPatches fails", async () => {
+      jest.spyOn(console, "error");
+      applyPatches.mockRejectedValue("failure");
+      const { changeActivelySortedHeader } = renderer();
+      newSortDirection = "A";
+      const result = await changeActivelySortedHeader(header);
+      expect(applyPatches).toHaveBeenCalledTimes(1);
+      expect(result).toBeFalsy;
+      expect(global.console.error).toHaveBeenCalledTimes(1);
+    });
+  });
+});
