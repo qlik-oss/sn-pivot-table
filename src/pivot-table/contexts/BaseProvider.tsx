cb034fc7f4796c29b3c7f0cf3a2bcf9f823d1b48
Christofer Bengtsson <cbt@qlik.com>
2023-09-08 10:39:42 +0200

    feat: base provider

---
+++ src/pivot-table/contexts/BaseProvider.tsx
@@ -0,0 +1,34 @@
+import type { stardust } from "@nebula.js/stardust";
+import React, { createContext, useContext, useMemo } from "react";
+import type { App, Model } from "../../types/QIX";
+
+interface IBaseProvider {
+  model: Model;
+  app: App;
+  interactions: stardust.Interactions;
+}
+
+interface BaseProviderProps extends IBaseProvider {
+  children: JSX.Element | JSX.Element[];
+}
+
+const NOOP_BASE = {} as IBaseProvider;
+
+const BaseContext = createContext<IBaseProvider>(NOOP_BASE);
+
+export const useBaseContext = (): IBaseProvider => useContext(BaseContext);
+
+/**
+ * Provider that providers properties that are not expected to change very often
+ * during the lifecycle of the chart and does not need a layer of abstraction on
+ * top of them. This typically means properties coming from @nebula.js/stardust hooks.
+ *
+ * The whole purpose of this provider is to avoid prop-drilling those props.
+ */
+const BaseProvider = ({ model, app, interactions, children }: BaseProviderProps): JSX.Element => {
+  const props = useMemo(() => ({ model, app, interactions }), [app, interactions, model]);
+
+  return <BaseContext.Provider value={props}>{children}</BaseContext.Provider>;
+};
+
+export default BaseProvider;
