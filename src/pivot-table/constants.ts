282b4323193aba297b1d14dd579bff73a0c5643e
Christofer Bengtsson <cbt@qlik.com>
2023-09-19 12:33:41 +0200

    fix: restore max row count

--- src/pivot-table/constants.ts
+++ src/pivot-table/constants.ts
@@ -4,7 +4,7 @@ export const DEFAULT_CELL_HEIGHT = 24;
 export const DEFAULT_HEADER_CELL_HEIGHT = 32;
 export const DEFAULT_LINE_CLAMP = 1;
 
-export const MAX_ROW_COUNT = 150;
+export const MAX_ROW_COUNT = 15_000;
 export const MAX_COLUMN_COUNT = 15_000;
 export const LINE_HEIGHT_COEFFICIENT = 4 / 3;
 export const CELL_PADDING_HEIGHT = 8;
