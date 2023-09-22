25ea1160fc9b01d2903857c01166890157c0bfd0
Christofer Bengtsson <cbt@qlik.com>
2023-09-22 19:31:52 +0200

    fix: first draft

--- src/hooks/use-load-data-pages.ts
+++ src/hooks/use-load-data-pages.ts
@@ -1,5 +1,6 @@
 import { useFetch } from "@qlik/nebula-table-utils/lib/hooks";
 import { DEFAULT_PAGE_SIZE, Q_PATH } from "../constants";
+import { MAX_COLUMN_COUNT } from "../pivot-table/constants";
 import type { Model, PivotLayout } from "../types/QIX";
 import type { LayoutService, PageInfo, ViewService } from "../types/types";
 
@@ -10,18 +11,12 @@ interface Props {
   pageInfo: PageInfo;
 }
 
-export const shouldFetchAdditionalData = (
-  qLastExpandedPos: EngineAPI.INxCellPosition | undefined,
-  viewService: ViewService,
-) => {
+export const shouldFetchAdditionalData = (qLastExpandedPos: EngineAPI.INxCellPosition | undefined) => {
   if (!qLastExpandedPos) {
     return false;
   }
 
-  return (
-    viewService.gridColumnStartIndex + viewService.gridWidth >= DEFAULT_PAGE_SIZE ||
-    viewService.gridRowStartIndex + viewService.gridHeight >= DEFAULT_PAGE_SIZE
-  );
+  return qLastExpandedPos.qx >= DEFAULT_PAGE_SIZE || qLastExpandedPos.qy >= DEFAULT_PAGE_SIZE;
 };
 
 export const isMissingLayoutData = (layout: PivotLayout, pageInfo: PageInfo): boolean => {
@@ -34,6 +29,60 @@ export const isMissingLayoutData = (layout: PivotLayout, pageInfo: PageInfo): bo
   if (qTop < pageInfo.page * pageInfo.rowsPerPage) return true;
   // otherwise check if we are missing data
   return qWidth < Math.min(DEFAULT_PAGE_SIZE, qSize.qcx) || qHeight < Math.min(DEFAULT_PAGE_SIZE, qSize.qcy);
+};
+
+/**
+ * Note: the state of the viewService is always from the last time the chart was rendered. So it is not
+ * guaranteed to work with a new layout, as its properties my no longer be valid.
+ */
+export const getFetchArea = (
+  qLastExpandedPos: EngineAPI.INxCellPosition | undefined,
+  viewService: ViewService,
+  qSize: EngineAPI.ISize,
+  pageInfo: PageInfo,
+) => {
+  const pageStartTop = pageInfo.page * pageInfo.rowsPerPage;
+  const pageEndTop = pageStartTop + pageInfo.rowsPerPage;
+
+  if (qSize.qcy < pageStartTop) {
+    /**
+     * Do not fetch data that does not exist. This can happen because "PageInfo" is resolved from the layout.
+     * Which means it will not be updated until the next render cycle.
+     */
+    return null;
+  }
+
+  // TODO Make sure to index vs size are correct, i.e. to -1 or not
+
+  // TODO do qLastExpandedPos neeed to know if it was from Left or Top grid?
+
+  let qLeft = 0;
+  let qTop = 0;
+
+  // qLastExpandedPos only exist in the layout if a new layout was received because a node was expanded or collapsed
+  if (qLastExpandedPos) {
+    // gridColumnStartIndex might not exist anymore in the new expanded/collapsed layout
+    if (viewService.gridColumnStartIndex > qLastExpandedPos.qx) {
+      qLeft = Math.max(0, qLastExpandedPos.qx - DEFAULT_PAGE_SIZE);
+    } else {
+      qLeft = viewService.gridColumnStartIndex;
+    }
+
+    // pageStartTop + viewService.gridRowStartIndex might not exist anymore in the new expanded/collapsed layout
+    const inViewTop = pageStartTop + viewService.gridRowStartIndex;
+    if (inViewTop > qLastExpandedPos.qy) {
+      qTop = Math.max(0, qLastExpandedPos.qy - DEFAULT_PAGE_SIZE);
+    } else {
+      qTop = inViewTop;
+    }
+  }
+
+  return {
+    qLeft,
+    qTop,
+    qWidth: Math.min(MAX_COLUMN_COUNT - qLeft, qSize.qcx - qLeft, DEFAULT_PAGE_SIZE),
+    qHeight: Math.min(pageEndTop - qTop, qSize.qcy - qTop, DEFAULT_PAGE_SIZE),
+  };
 };
 
 const useLoadDataPages = ({ model, layoutService, viewService, pageInfo }: Props) => {
@@ -52,19 +101,21 @@ const useLoadDataPages = ({ model, layoutService, viewService, pageInfo }: Props
     if (
       model !== undefined &&
       "getHyperCubePivotData" in model &&
-      (shouldFetchAdditionalData(qLastExpandedPos, viewService) || isMissingLayoutData(layout, pageInfo))
+      (shouldFetchAdditionalData(qLastExpandedPos) || isMissingLayoutData(layout, pageInfo))
     ) {
-      const fetchArea: EngineAPI.INxPage = {
-        qLeft: qLastExpandedPos ? viewService.gridColumnStartIndex : 0,
-        qTop: pageInfo.page * pageInfo.rowsPerPage + (qLastExpandedPos ? viewService.gridRowStartIndex : 0),
-        qWidth: !viewService.gridWidth ? DEFAULT_PAGE_SIZE : viewService.gridWidth,
-        qHeight: !viewService.gridHeight ? DEFAULT_PAGE_SIZE : viewService.gridHeight,
-      };
-      return model.getHyperCubePivotData(Q_PATH, [fetchArea]);
+      const fetchArea = getFetchArea(qLastExpandedPos, viewService, layout.qHyperCube.qSize, pageInfo);
+      console.log("%c fetchArea", "color: orangered", {
+        fetchArea,
+        viewService: { ...viewService },
+        pageInfo,
+        qLastExpandedPos,
+      });
+      return fetchArea ? model.getHyperCubePivotData(Q_PATH, [fetchArea]) : [];
     }
 
     return qHyperCube.qPivotDataPages ?? [];
     // By explicitly using layout, isSnapshot, pageInfo.page and pageInfo.rowsPerPage in the deps list. Two re-dundent page fetches are skipped on first render
   }, [layout, isSnapshot, model, viewService, pageInfo.page, pageInfo.rowsPerPage]);
 };
+
 export default useLoadDataPages;
