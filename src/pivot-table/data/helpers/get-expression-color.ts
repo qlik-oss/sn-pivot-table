22c254a7232c5dc0e5e5287fd1e2b748ce5c7dbf
Christofer Bengtsson <cbt@qlik.com>
2023-10-06 09:37:53 +0200

    chore: add comment

--- src/pivot-table/data/helpers/get-expression-color.ts
+++ src/pivot-table/data/helpers/get-expression-color.ts
@@ -6,6 +6,7 @@ const getExpressionColor = (
   attrsExprInfo: AttrExprInfoIndex,
   cell: EngineAPI.INxPivotValuePoint | EngineAPI.INxPivotDimensionCell,
 ) => {
+  // Cast to object type as cell.qAttrExps is EngineAPI.INxAttributeExpressionValues | EngineAPI.INxAttributeExpressionValues[]
   const attrsExprValues = cell.qAttrExps as unknown as EngineAPI.INxAttributeExpressionValues;
   const expressionColor = attrsExprValues?.qValues?.[attrsExprInfo.cellForegroundColor]?.qText;
   const expressionBackground = attrsExprValues?.qValues?.[attrsExprInfo.cellBackgroundColor]?.qText;
