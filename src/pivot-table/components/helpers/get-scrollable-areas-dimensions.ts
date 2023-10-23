07482f71562d19e3779e5d8f7e486b50405550e8
Ahmad Mirzaei <a.mirzaei.dev@gmail.com>
2023-10-23 12:57:42 +0200

    fix: overlay scrollbars on data (#417)
    * wip: fix data grid border issue
    
    * fix: pvt hanging in middle of the page
    
    * chore: snap scrollbar below content
    
    * fix: vertical scroll over data grid fixed
    
    * fix: vertical scrollbar overlay
    
    * chore: cleanup + get horzontal scrollbar height
    
    * chore: fix horizontal scrollbar overlay when page is full
    
    * fix: last page scrollbar overlay on data
    
    * chore: include horizontal scrollbar height in calculations
    
    * chore: separate refs for child scrollable containers
    
    * chore: scrollbar height is now reacts to resizing
    
    * fix: leftGridRef misses list elements when expand collapse -> onVerticalScrollHandler throws error and data grid fails to scroll
    
    * fix: update horizontalScrollbar height based on all columns visible or not
    
    * chore: generalize calculations into variabels for getScrollableAreDimension
    
    * test: add unit tests 
    
    * chore: use already computed sumOfWidths
    
    * chore: review comments

---
+++ src/pivot-table/components/helpers/get-scrollable-areas-dimensions.ts
@@ -0,0 +1,126 @@
+import type { Rect } from "../../../types/types";
+import { GRID_BORDER } from "../../constants";
+
+type ContainerDims = {
+  scrollable: Rect;
+  fullSize: Rect;
+  sticky: Rect;
+};
+
+interface GetScrollableAreasDimensionsProps {
+  tableRect: Rect;
+
+  containerHeight: number;
+  leftGridHeight: number;
+  topGridHeight: number;
+  dataGridHeight: number;
+  allRowsVisible: boolean;
+
+  totalWidth: number;
+  leftGridWidth: number;
+  rightGridWidth: number;
+  verticalScrollbarWidth: number;
+  horizontalScrollbarHeight: number;
+}
+
+interface GetScrollableAreasDimensionsResult {
+  rootWrapper: ContainerDims;
+  leftWrapper: {
+    containers: ContainerDims;
+    headerGrid: Rect;
+    leftGrid: Rect;
+  };
+  rightWrapper: {
+    containers: ContainerDims;
+    topGrid: Rect;
+    dataGrid: Rect;
+  };
+}
+
+const getScrollableAreasDimensions = ({
+  tableRect,
+
+  containerHeight,
+  leftGridHeight,
+  topGridHeight,
+  dataGridHeight,
+  allRowsVisible,
+
+  totalWidth,
+  leftGridWidth,
+  rightGridWidth,
+  verticalScrollbarWidth,
+  horizontalScrollbarHeight,
+}: GetScrollableAreasDimensionsProps): GetScrollableAreasDimensionsResult => {
+  const modifiedVerticalScrollbarWidth = verticalScrollbarWidth * (allRowsVisible ? 0 : 1);
+  const modifiedHorizontalScrollbarHeight = horizontalScrollbarHeight * (allRowsVisible ? 0 : 1);
+
+  const childWrappersScrollableContainerHeight = allRowsVisible
+    ? topGridHeight + dataGridHeight + GRID_BORDER + horizontalScrollbarHeight
+    : tableRect.height;
+  const childWrappersStickyContainerHeight = tableRect.height - modifiedHorizontalScrollbarHeight;
+
+  return {
+    rootWrapper: {
+      scrollable: tableRect,
+      fullSize: {
+        width: totalWidth - modifiedVerticalScrollbarWidth,
+        height: containerHeight + modifiedHorizontalScrollbarHeight,
+      },
+      sticky: {
+        width: tableRect.width - modifiedVerticalScrollbarWidth,
+        height: tableRect.height,
+      },
+    },
+    leftWrapper: {
+      containers: {
+        scrollable: {
+          width: leftGridWidth,
+          height: childWrappersScrollableContainerHeight,
+        },
+        fullSize: {
+          width: leftGridWidth,
+          height: containerHeight,
+        },
+        sticky: {
+          width: leftGridWidth,
+          height: childWrappersStickyContainerHeight,
+        },
+      },
+      headerGrid: {
+        width: -1,
+        height: topGridHeight,
+      },
+      leftGrid: {
+        width: leftGridWidth,
+        height: leftGridHeight - modifiedHorizontalScrollbarHeight,
+      },
+    },
+    rightWrapper: {
+      containers: {
+        scrollable: {
+          width: rightGridWidth + GRID_BORDER - modifiedVerticalScrollbarWidth,
+          height: childWrappersScrollableContainerHeight,
+        },
+        fullSize: {
+          width: totalWidth - leftGridWidth - modifiedVerticalScrollbarWidth,
+          height: containerHeight,
+        },
+        sticky: {
+          width: tableRect.width - leftGridWidth - modifiedVerticalScrollbarWidth,
+          height: childWrappersStickyContainerHeight,
+        },
+      },
+      topGrid: {
+        width: rightGridWidth - modifiedVerticalScrollbarWidth,
+        height: topGridHeight,
+      },
+      dataGrid: {
+        width: rightGridWidth - modifiedVerticalScrollbarWidth,
+        height: dataGridHeight - modifiedHorizontalScrollbarHeight,
+      },
+    },
+  };
+};
+
+export default getScrollableAreasDimensions;
