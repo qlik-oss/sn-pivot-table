80b1fd08c133046f842d8b637af2b0cbb7df68be
a-m-dev <a.mirzaei.dev@gmail.com>
2023-10-24 09:01:52 +0200

    chore: correct width for icon

--- src/pivot-table/components/icons/styles.ts
+++ src/pivot-table/components/icons/styles.ts
@@ -1,7 +1,6 @@
-import { HEADER_ICON_SIZE } from "../../constants";
 import { baseFlex } from "../shared-styles";
 
 export const headerPlusMinusIconWrapperStyle = {
-  width: HEADER_ICON_SIZE,
+  width: 13,
   ...baseFlex,
 };
