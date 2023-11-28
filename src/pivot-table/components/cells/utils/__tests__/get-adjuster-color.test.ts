5680a59457d50e965ad6ff04367ae218203156a2
Max Gefvert <max.gefvert@qlik.com>
2023-11-28 11:15:56 +0100

    test: add test

--- src/pivot-table/components/cells/utils/__tests__/get-adjuster-color.test.ts
+++ src/pivot-table/components/cells/utils/__tests__/get-adjuster-color.test.ts
@@ -21,24 +21,35 @@ describe("get-adjuster-color", () => {
     isHeader = true;
   });
 
-  test("should return default adjuster color for header", () => {
+  test("should return blended color for header with default styling", () => {
     const color = getAdjusterColor(styleService, isHeader);
     expect(color).toBe("rgb(216.75, 216.75, 216.75)");
   });
-  test("should return default adjuster color for top grid", () => {
+
+  test("should return blended color for top grid with default styling", () => {
     isHeader = false;
 
     const color = getAdjusterColor(styleService, isHeader);
     expect(color).toBe("rgb(216.75, 216.75, 216.75)");
   });
-  test("should return default adjuster color for non-transparent background", () => {
+
+  test("should return blended adjuster color for non-transparent background", () => {
     styleService.header.background = "#ff0000";
 
     const color = getAdjusterColor(styleService, isHeader);
     expect(color).toBe("rgb(216.75, 0, 0)");
   });
-  test("should return default adjuster color for opaque border color", () => {
-    styleService.grid.border = "#00ff00";
+
+  test("should return blended adjuster color for specified translucent border and opaque background", () => {
+    styleService.header.background = "#ff0000";
+    styleService.grid.border = "rgba(0, 0, 0, 0.5)";
+
+    const color = getAdjusterColor(styleService, isHeader);
+    expect(color).toBe("rgb(127.5, 0, 0)");
+  });
+
+  test("should return unchanged adjuster color for opaque border color", () => {
+    styleService.grid.border = "#ff0000";
 
     const color = getAdjusterColor(styleService, isHeader);
     expect(color).toBe(styleService.grid.border);
