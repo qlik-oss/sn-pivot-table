01baf3e89f3b2827e4c81b1b4ae697bb4f0309bc
Max Gefvert <max.gefvert@qlik.com>
2023-12-01 10:59:13 +0100

    fix: undo undo

--- src/pivot-table/hooks/use-column-width/constants.ts
+++ src/pivot-table/hooks/use-column-width/constants.ts
@@ -3,9 +3,9 @@ import { GRID_BORDER, HEADER_ICON_SIZE, PLUS_MINUS_ICON_SIZE } from "../../const
 
 // Size of the icon + left/right padding on icon
 export const EXPAND_ICON_SIZE = PLUS_MINUS_ICON_SIZE + DOUBLE_CELL_PADDING;
-// CELL_PADDING as grid gap between header text and menu/lock icon
+// DOUBLE_CELL_PADDING as grid gap between header text and menu/lock icon
 export const MENU_ICON_SIZE = HEADER_ICON_SIZE + DOUBLE_CELL_PADDING;
 export const LOCK_ICON_SIZE = MENU_ICON_SIZE;
 // Left and right padding + potential border + 1 px extra buffer
-export const TOTAL_CELL_PADDING = DOUBLE_CELL_PADDING * 2 + GRID_BORDER;
+export const TOTAL_CELL_PADDING = DOUBLE_CELL_PADDING * 2 + GRID_BORDER * 2;
 export const LEFT_GRID_MAX_WIDTH_RATIO = 0.75;
